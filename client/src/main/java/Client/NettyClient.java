b9dc246be32a280082de21af14afb8c0f46af484
Александр <trigun1000@mail.ru>
2020-12-19 22:23:55 +0300

    les2

---
+++ client/src/main/java/Client/NettyClient.java
@@ -0,0 +1,38 @@
+package Client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.*;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.serialization.ClassResolvers;
+import io.netty.handler.codec.serialization.ObjectDecoder;
+import io.netty.handler.codec.serialization.ObjectEncoder;
+
+public class NettyClient {
+
+    public static void main(String[] args) throws Exception{
+        String host = "localhost";
+        int port = 8080;
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(workerGroup);
+            b.channel(NioSocketChannel.class);
+            b.option(ChannelOption.SO_KEEPALIVE, true);
+            b.handler(new ChannelInitializer<SocketChannel>(){
+                @Override
+                        public void initChannel(SocketChannel ch) {
+                    ch.pipeline().addLast(new ObjectEncoder(), new ObjectDecoder(150*1024*1024,
+                            ClassResolvers.cacheDisabled(null)),new ClientHandler());
+                }
+            });
+            ChannelFuture channelFuture = b.connect(host, port).sync();
+
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
