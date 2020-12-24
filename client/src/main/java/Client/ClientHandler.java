a112c0cf1dd23bd50a545c712fee8c5f6e89c652
Александр <trigun1000@mail.ru>
2020-12-24 16:10:54 +0300

    les3

--- client/src/main/java/Client/ClientHandler.java
+++ client/src/main/java/Client/ClientHandler.java
@@ -3,6 +3,19 @@ package Client;
 
 import io.netty.channel.ChannelOutboundHandlerAdapter;
 
+import java.util.concurrent.CountDownLatch;
+
 public class ClientHandler extends ChannelOutboundHandlerAdapter {
+
+    private final ClientHandler clientHandler;
+
+    public ClientHandler(ClientHandler clientHandler) {
+        this.clientHandler = clientHandler;
+    }
+
+    CountDownLatch NettyClient = new CountDownLatch(1);
+    ClientHandler clientHandler = new ClientHandler(NettyClient);
+
+
 
 }
